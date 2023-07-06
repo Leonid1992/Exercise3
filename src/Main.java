fc77b5848cd8323942b66a51a0c676c7ab58aa6f
Leonid <leonid.vilcu@vlc2.com>
2023-07-06 15:57:55 +0200

    Exercise3 finito

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+//Create an array and initialize it with numbers, 1-10.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeri = {1,2,3,4,5,6,7,8,9,10};
+    }
+}
